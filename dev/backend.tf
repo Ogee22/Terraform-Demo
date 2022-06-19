721d6ced5fabf85c50ac2a3231dd7a1d028cd496
oluwoleatiks <oluwole.atiks@gmail.com>
2022-06-19 22:13:35 +0100

    Update backend.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "azurerm" {
-#     storage_account_name = "tfdemostorageacc"
-#     container_name       = "terraform-state"
-#     key                  = "tfazdemokey001.tfstate"
+     storage_account_name = "tfdemostorageacc"
+     container_name       = "terraform-state"
+     key                  = "tfazdemokey001.tfstate"
+     access_key = "jkogPrn9fnbXirgWATRwOwIflgkodPzRNzrv4qULIgLmNPyREiGKO4+rmBaL3F8N2fndvllc4vFw+AStRaqxWA=="
   }
 }
