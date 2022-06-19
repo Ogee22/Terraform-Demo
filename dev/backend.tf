6fe04081fe108624139dfb44c0e5e844e1d101d9
Oluwole Atiku <oluwole.atiks@gmail.com>
2022-06-19 17:59:26 +0100

    justed some parameters

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,4 +1,7 @@
 terraform {
   backend "azurerm" {
+    storage_account_name = "tfdemostorageacc"
+    container_name       = "terraform-state"
+    key                  = "tfazdemokey001.tfstate"
   }
 }
